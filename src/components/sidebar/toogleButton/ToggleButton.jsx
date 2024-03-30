12aa49c843db2c80f9fa83c2b6f0a938d1014541
assilbekov <askhat.assilbekov@gmail.com>
2024-03-30 13:04:44 -0300

    ToggleButton init svg

--- src/components/sidebar/toogleButton/ToggleButton.jsx
+++ src/components/sidebar/toogleButton/ToggleButton.jsx
@@ -3,7 +3,11 @@ import "./toggleButton.scss"
 export const ToggleButton = ({ setOpen }) => {
   return (
     <button onClick={() => setOpen(v => !v)}>
-      Toogle button
+      <svg width="23" height="23" viewBox="0 0 23 23">
+        <path strokeWidth="3" stroke="black" strokeLinecap="round" />
+        <path strokeWidth="3" stroke="black" strokeLinecap="round" />
+        <path strokeWidth="3" stroke="black" strokeLinecap="round" />
+      </svg>
     </button>
   )
 }
