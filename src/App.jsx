48d99f5a494789b174b00585f3586ab9df6abe63
assilbekov <askhat.assilbekov@gmail.com>
2024-03-30 13:02:35 -0300

    nav added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ function App() {
 
   return (
     <div>
-      <section>
+      <section id="Homepage">
         <Navbar />
       </section>
-      <section>Parallax</section>
-      <section>Services</section>
-      <section>Parallax</section>
+      <section id="Services">Parallax</section>
+      <section id="Portfolio">Services</section>
+      <section id="About">Parallax</section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   )
 }
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,9 +1,19 @@
 import "./links.scss";
 
 export const Links = () => {
+  const items = [
+    "Homepage",
+    "Services",
+    "Portfolio",
+    "Contact",
+    "About",
+  ]
+
   return (
     <div className="links">
-      links
+      {items.map((item, index) => (
+        <a href={`#${item}`} key={index}>{item}</a>
+      ))}
     </div>
   )
 }
