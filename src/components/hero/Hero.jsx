27f7a481f246a886950af356525a60301f83b9b1
assilbekov <askhat.assilbekov@gmail.com>
2024-03-30 13:28:52 -0300

    add hero copy

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,6 +3,18 @@ import "./hero.scss"
 export const Hero = () => {
   return (
     <div className="hero">
+      <div className="wrapper">
+
+        <div className="textContainer">
+          <h2>Harvey Asssilbekov</h2>
+          <h1>Web developer, Frontend and Backend</h1>
+          <div className="buttons">
+            <button>See the latest works</button>
+            <button>Contact me</button>
+          </div>
+          <img src="/scroll.png" alt="down arrow" />
+        </div>
+      </div>
       <div className="imageContainer">
         <img src="/hero.png" alt="hero" />
       </div>
