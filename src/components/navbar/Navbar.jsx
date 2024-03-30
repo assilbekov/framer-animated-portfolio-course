b3a2868bafe2ca8a8ff2bfd65e422a2bd8652e84
assilbekov <askhat.assilbekov@gmail.com>
2024-03-30 09:04:31 -0300

    style navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,17 @@ import "./navbar.scss"
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      {/* Sidebar */}
+      <div className="wrapper">
+        <span>Lama dev</span>
+        <div className="social">
+          <a href="#home"><img src="facebook.png" /></a>
+          <a href="#home"><img src="instagram.png" /></a>
+          <a href="#home"><img src="youtube.png" /></a>
+          <a href="#home"><img src="dribbble.png" /></a>
+        </div>
+      </div>
+    </div>
   )
 }
