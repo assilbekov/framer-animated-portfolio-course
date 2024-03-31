73ea54fb7a68f8deeeb65cacaf0c50436af36bb9
assilbekov <askhat.assilbekov@gmail.com>
2024-03-31 05:06:56 -0300

    comment cursor

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div>
-      <Cursor />
+      {/* <Cursor /> */}
       <section id="Homepage">
         <Navbar />
         <Hero />
--- src/components/cursor/Cursor.jsx
+++ src/components/cursor/Cursor.jsx
@@ -20,7 +20,7 @@ export const Cursor = () => {
   return (
     <motion.div
       className="cursor"
-      animate={{ x: position.x + 10, y: position.y + 10 }}
+      animate={{ x: position.x, y: position.y }}
     ></motion.div>
   );
 }
