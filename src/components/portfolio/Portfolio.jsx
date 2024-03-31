f6f9b025a3bc998393406a30e100bb410cd9485d
assilbekov <askhat.assilbekov@gmail.com>
2024-03-31 03:48:05 -0300

    add portfolio items

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,32 @@
 import "./portfolio.scss";
 
+const items = [
+  {
+    id: 1,
+    title: "React Commerce",
+    img: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate molestiae ea corrupti pariatur ducimus facilis dignissimos officiis veritatis hic dolores, beatae consequuntur reprehenderit tenetur quo eum ab. Ab, repellendus aliquid?"
+  },
+  {
+    id: 2,
+    title: "Nextjs Commerce",
+    img: "https://images.pexels.com/photos/17554347/pexels-photo-17554347/free-photo-of-taxis-in-front-of-palace-in-istanbul.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate molestiae ea corrupti pariatur ducimus facilis dignissimos officiis veritatis hic dolores, beatae consequuntur reprehenderit tenetur quo eum ab. Ab, repellendus aliquid?"
+  },
+  {
+    id: 3,
+    title: "Vanilla JS APP",
+    img: "https://media.gettyimages.com/id/1982991174/pt/foto/summer-breeze.jpg?s=1024x1024&w=gi&k=20&c=LwpmjiTYcuSgo4r1jag9cMy_NsonWbI3BckDz-sX_rY=",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate molestiae ea corrupti pariatur ducimus facilis dignissimos officiis veritatis hic dolores, beatae consequuntur reprehenderit tenetur quo eum ab. Ab, repellendus aliquid?"
+  },
+  {
+    id: 4,
+    title: "Music app",
+    img: "https://images.pexels.com/photos/19845821/pexels-photo-19845821/free-photo-of-sheep-on-a-hillside-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate molestiae ea corrupti pariatur ducimus facilis dignissimos officiis veritatis hic dolores, beatae consequuntur reprehenderit tenetur quo eum ab. Ab, repellendus aliquid?"
+  },
+]
+
 export const Portfolio = () => {
   return (
     <div>
