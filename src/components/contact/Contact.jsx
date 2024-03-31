46c397c4d938dd4648fb385f697b0814e08deca8
assilbekov <askhat.assilbekov@gmail.com>
2024-03-31 04:49:50 -0300

    update form contacts

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -18,7 +18,7 @@ const variants = {
 
 export const Contact = () => {
   return (
-    <motion.div className="contact" variants={variants}>
+    <motion.div className="contact" variants={variants} initial="initial" whileInView="animate">
       <motion.div className="textContainer" variants={variants}>
         <motion.h1 variants={variants}>Let's work together</motion.h1>
         <motion.div variants={variants} className="item">
