3e451c605985e564ee125f940eef76eda92c977b
assilbekov <askhat.assilbekov@gmail.com>
2024-03-31 05:02:14 -0300

    add cursor style and imp

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./app.scss"
 import { Contact } from "./components/contact/Contact"
+import { Cursor } from "./components/cursor/Cursor"
 import { Hero } from "./components/hero/Hero"
 import { Navbar } from "./components/navbar/Navbar"
 import { Parallax } from "./components/parallax/Parallax"
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <div>
-      {/* <Cursor /> */}
+      <Cursor />
       <section id="Homepage">
         <Navbar />
         <Hero />
