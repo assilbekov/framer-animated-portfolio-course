f765097529042e69cf9eadb3f7f15ba3b1a9f957
assilbekov <askhat.assilbekov@gmail.com>
2024-03-31 04:05:44 -0300

    implement portfolio style

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import "./portfolio.scss";
 import { useRef } from "react";
 
@@ -30,11 +30,26 @@ const items = [
 ]
 
 const Single = ({ item }) => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300])
+
   return (
-    <section>
-      {item.title}
+    <section >
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export const Portfolio = () => {
