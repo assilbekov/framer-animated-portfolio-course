fb164df15b93434f43ffd201f3f5c57470809ac2
assilbekov <askhat.assilbekov@gmail.com>
2024-03-31 03:21:51 -0300

    animate services page

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,16 +1,37 @@
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import "./services.scss";
+import { useRef } from "react";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    }
+  }
+}
 
 export const Services = () => {
+  const ref = useRef();
+  const isInView = useInView(ref, { margin: "-100px" });
+
   return (
-    <motion.div className="services">
-      <motion.div className="textContainer">
+    <motion.div ref={ref} className="services" variants={variants} initial="initial" animate={isInView && "animate"}>
+      <motion.div className="textContainer" variants={variants}>
         <p>
           I focus on your brand grow <br /> and move forward
         </p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div variants={variants} className="titleContainer">
         <div className="title">
           <img src="/people.webp" alt="people" />
           <h1><b>Unique</b> Ideas</h1>
@@ -20,7 +41,7 @@ export const Services = () => {
           <button>WHAT WE DO?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
+      <motion.div className="listContainer" variants={variants}>
         <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
           <h2>Branding</h2>
           <p>
